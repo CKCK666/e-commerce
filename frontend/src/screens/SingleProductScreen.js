32f9533402445215a872ac2f46be5bf1f088d6fd
unknown <you@example.com>
2022-04-28 11:34:34 +0530

    home and product screens state manage with REDUX

--- frontend/src/screens/SingleProductScreen.js
+++ frontend/src/screens/SingleProductScreen.js
@@ -1,36 +1,40 @@
-import React, { useEffect ,useState} from 'react'
+import React, { useEffect } from 'react'
 import {Link } from "react-router-dom"
  import {Col,Row,ListGroup,Button,Card,Image} from "react-bootstrap"
 
  import Rating from '../components/Rating'
 import {useParams} from "react-router-dom"
-import axios from 'axios'
+
+import { useDispatch, useSelector } from 'react-redux'
+import { listProductDetails } from '../actions/productAction'
+
 
 const SingleProductScreen = () => {
   const params=useParams()
-  const [product,setProduct]=useState([])
+  const dispatch=useDispatch()
+  const productDetails=useSelector(state=>state.productDetails)
+  const{loading,error,product}=productDetails
+ 
   useEffect(()=>{
-     const fetchProduct= async()=>{
-       const {data}= await axios.get(`/api/products/${params.id}`)
-       setProduct(data)
-     }
-     fetchProduct()
-  })
+    
+    dispatch(listProductDetails(params.id))
+  },[dispatch,params])
  
   return (
     <>
          
         <Link to="/" className='btn btn-secondary btn-lg my-3'>Go Back</Link>
+        {loading ? (<h2>Loading....</h2>) : error ? (<h2>{error}</h2>):
         <Row>
           <Col md={6}>
-            <Image src={process.env.PUBLIC_URL + `${product.image}`}      alt={product.name}/>
+            <Image src={process.env.PUBLIC_URL + `${product.image}`}      alt={product.name} fluid/>
           </Col>
           <Col md={3}>
-             <ListGroup variant='flush'>
-               <ListGroup.Item>
+             <ListGroup variant='flush' >
+               <ListGroup.Item >
                <h3>{product.name}</h3>
                </ListGroup.Item>
-               <ListGroup.Item><Rating value={product.rating}  text={`${product.numReviews} rating`}/></ListGroup.Item>
+               <ListGroup.Item><Rating value={product.rating}  text={`${product.numReview} rating`}/></ListGroup.Item>
                <ListGroup.Item>Price:{product.price}</ListGroup.Item>
                <ListGroup.Item>Description:{product.description}</ListGroup.Item>
               
@@ -74,6 +78,7 @@ const SingleProductScreen = () => {
             </Card>
           </Col>
         </Row>
+        }
        
     </>
   )
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,8 +1,12 @@
 import thunk from "redux-thunk"
 import {createStore ,combineReducers,applyMiddleware} from "redux"
 import{composeWithDevTools} from "redux-devtools-extension"
+import  { productDetailsReducer, productListReducer } from "./reducers/productReducer"
 
-const reducer=combineReducers({})
+const reducer=combineReducers({
+    productList:productListReducer, 
+    productDetails:productDetailsReducer
+})
 const initialState={}
 const middleware=[thunk]
 const store=createStore (reducer,initialState,composeWithDevTools(applyMiddleware(...middleware)))
